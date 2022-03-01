ba29664f6ce34162f561968bf952f9a85cb895d4
wins <win4709hi@gmail.com>
2022-03-01 23:58:12 +0800

    feat: init project

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex flex-row justify-center text-green-500 font-bold">pages - index.vue</div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "index"
+}
+</script>
